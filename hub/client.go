8ed9ffc1c3e10af7f2f187ae9a9917eeab037ed9
MrJosh <josheghani.dev@gmail.com>
2020-04-06 00:36:08 +0430

    Moveing sending ping request from authorized method to start of Listen method

--- hub/client.go
+++ hub/client.go
@@ -86,6 +86,7 @@ func (c *Client) PingPongHandler() {
 			if diff.Round(time.Second) > time.Minute {
 				c.ctxCancel()
 				c.Close()
+				return
 			}
 		case <-c.pingChan:
 			c.lastPingAt = time.Now()
@@ -105,6 +106,8 @@ func (c *Client) Listen() error {
 		log.Printf("[%d] End of events!", c.Id)
 		close(c.Event)
 	}()
+
+	c.pingChan <- struct{}{}
 
 	for {
 
@@ -137,7 +140,6 @@ func (c *Client) Listen() error {
 						return c.Close()
 					}
 					log.Printf("[%d] Authorized!", c.Id)
-					c.pingChan <- struct{}{}
 				}
 			}
 
