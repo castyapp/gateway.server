7450006fed13d37cfd41b025ce8df52e4f3e19c2
MrJosh <josheghani.dev@gmail.com>
2020-04-05 22:39:56 +0430

    Removing counting go routine debug code form server.go

--- client/main.go
+++ client/main.go
@@ -13,13 +13,14 @@ func main() {
 	accessToken := "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE1ODYzMTYxNjEsInN1YiI6IjVlODdjNWU1MWI2YmFiOTYyYzg5ZmM5MiJ9.jFwkMoxs4JW1deL96khANhO-m1EFQlB6syCskV9jkbhacUKgy2_4qpoJ9J4z3Fi1XkOaDd1y8zx4TAddc0F4xMUyonO3W4nhjSROblP16ak3Mh5i-_g4WSZoTN7H32v2H2bwR_GiHTZcM4i5Pxdk34JGg_zgKlT5Qm72vzEUKlLfgdMP7sjZsQJkxfztWRtwotahimz3W6U4AFT-0Qfy6CY6M14erqgc6cDA_bhlVvt0s9ZSYvAFd5sI3clxSpiQ35EHDlzlHwB6qVV8BxVtkc7dw8oGZC6mLSO8WV8v4cIm6mwKfhFRahiiiREL0FE51cEH0J6S2p6iANK32RrzKGRZ_7HaS5W_BrDNxXiabCtfkduhryu4mezmRIpgNslMd8I3MFLZqYGkYSRQTETp2wJKtlMgxxs1kSvNXESPDTFvC3r5RA355keBU_fhMi8EMOnn6lFzYvXosu2cDy8lnBB5jejX1rKyBccjEBy3QXS1Xid9-Sxz7OEDO0Opd3XLLsokTMzeDHlbOIJGgmb4ljKZ2zmB22nNtZPLMOvpU16I9ICyelRtqP89pwvIRVROFyzOzDbNDrDZ1Q9gP1slObUfHJJ4g4pd0wyH3jCoc1yX5fcjRZ71t-peD_A-KwaAaxwyjv_6p4q5hk6b6DBSKrbkkI6j0fkka3m1l_ch1R8"
 
 	websocket.DefaultDialer.Subprotocols = []string{"cp0", "cp1"}
-	clientConn, _, err := websocket.DefaultDialer.Dial("ws://127.0.0.1:3000/user", nil)
+	clientConn, _, err := websocket.DefaultDialer.Dial("ws://127.0.0.1:3000/theater", nil)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	buffer, err := protocol.NewMsgProtobuf(proto.EMSG_LOGON, &proto.LogOnEvent{
+	buffer, err := protocol.NewMsgProtobuf(proto.EMSG_LOGON, &proto.TheaterLogOnEvent{
 		Token: []byte(accessToken),
+		Room: []byte("5e87caef1b6bab962c89fc9e"),
 	})
 	if err != nil {
 		log.Fatal(err)
--- hub/theater_hub.go
+++ hub/theater_hub.go
@@ -21,51 +21,57 @@ type TheaterHub struct {
 	cmap     cmap.ConcurrentMap
 }
 
-func (h *TheaterHub) GetContext() context.Context {
-	return h.ctx
+func (hub *TheaterHub) WithContext(ctx context.Context) {
+	hub.ctx = ctx
 }
 
-func (h *TheaterHub) FindRoom(name string) (room Room, err error) {
-	if r, ok := h.cmap.Get(name); ok {
+func (hub *TheaterHub) GetContext() context.Context {
+	return hub.ctx
+}
+
+func (hub *TheaterHub) FindRoom(name string) (room Room, err error) {
+	if r, ok := hub.cmap.Get(name); ok {
 		return r.(*TheaterRoom), nil
 	}
 	return nil, errors.New("theater room is missing from cmp")
 }
 
 /* If room doesn't exist creates it then returns it */
-func (h *TheaterHub) GetOrCreateRoom(name string) (room Room) {
-	if r, ok := h.cmap.Get(name); ok {
+func (hub *TheaterHub) GetOrCreateRoom(name string) (room Room) {
+	if r, ok := hub.cmap.Get(name); ok {
 		return r.(*TheaterRoom)
 	}
-	room, _ = NewTheaterRoom(name, h)
-	h.cmap.Set(name, room)
+	room, _ = NewTheaterRoom(name, hub)
+	hub.cmap.Set(name, room)
 	return
 }
 
 /* If room doesn't exist creates it then returns it */
-func (h *TheaterHub) GetRoom(name string) (*TheaterRoom, error) {
-	if !h.cmap.Has(name) {
+func (hub *TheaterHub) GetRoom(name string) (*TheaterRoom, error) {
+	if !hub.cmap.Has(name) {
 		return nil, errors.New("room not found")
 	}
-	if r, ok := h.cmap.Get(name); ok {
+	if r, ok := hub.cmap.Get(name); ok {
 		return r.(*TheaterRoom), nil
 	}
 	return nil, errors.New("room is missing from cmp")
 }
 
-func (h *TheaterHub) RemoveRoom(name string) {
-	h.cmap.Remove(name)
+func (hub *TheaterHub) RemoveRoom(name string) {
+	hub.cmap.Remove(name)
 	return
 }
 
-func (h *TheaterHub) Close() error {
+func (hub *TheaterHub) Close() error {
 	return nil
 }
 
 /* Get ws conn. and hands it over to correct room */
-func (h *TheaterHub) Handler(w http.ResponseWriter, req *http.Request) {
+func (hub *TheaterHub) Handler(w http.ResponseWriter, req *http.Request) {
 
-	h.ctx = req.Context()
+	hub.WithContext(req.Context())
+
+	// Upgrade connection to websocket
 	conn, _, _, err := ws.UpgradeHTTP(req, w)
 	if err != nil {
 		sentry.CaptureException(err)
@@ -73,16 +79,23 @@ func (h *TheaterHub) Handler(w http.ResponseWriter, req *http.Request) {
 		return
 	}
 
-	client := NewTheaterClient(h, conn)
+	// Create a new client for user
+	client := NewTheaterClient(hub, conn)
+
+	log.Printf("[%d] New client connected", client.Id)
+
+	// Close connection after client disconnected
 	defer client.Close()
 
+	// Join user room if client recieved authorized
 	client.OnAuthorized(func(auth Auth) (room Room) {
 		event := auth.Event().(*proto.TheaterLogOnEvent)
-		room = h.GetOrCreateRoom(string(event.Room))
+		room = hub.GetOrCreateRoom(string(event.Room))
 		room.Join(client)
 		return
 	})
 
+	// Send Unauthorized message to client if client Unauthorized and close connection
 	client.OnUnauthorized(func() {
 		buffer, err := protocol.NewMsgProtobuf(proto.EMSG_UNAUTHORIZED, nil)
 		if err == nil {
@@ -90,6 +103,7 @@ func (h *TheaterHub) Handler(w http.ResponseWriter, req *http.Request) {
 		}
 	})
 
+	// Listen on client events
 	client.Listen()
 	return
 }
