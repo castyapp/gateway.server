0924e6f7355e5f892067581ed3a98b263a79b5d8
Alireza Josheghani <josheghani.dev@gmail.com>
2020-10-24 08:17:14 +0330

    adding SentinelAddrs to debugging

--- redis/redis.go
+++ redis/redis.go
@@ -14,12 +14,13 @@ var (
 func Configure() error {
 	Client = redis.NewFailoverClient(&redis.FailoverOptions{
 		SentinelAddrs: config.Map.Secrets.Redis.Sentinels,
-		Password: config.Map.Secrets.Redis.Pass,
+		SentinelPassword: config.Map.Secrets.Redis.Pass,
 		MasterName: config.Map.Secrets.Redis.MasterName,
 		DB: 0,
 	})
 	cmd := Client.Ping(context.Background())
 	if res := cmd.Val(); res != "PONG" {
+		log.Println("SentinelAddrs: ", config.Map.Secrets.Redis.Sentinels)
 		log.Fatalf("Could not ping the redis server: %v", cmd.Err())
 	}
 	return nil
