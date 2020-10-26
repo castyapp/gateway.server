29c965d01389d9c154f2d78bd70a482723087663
Alireza Josheghani <josheghani.dev@gmail.com>
2020-10-26 05:43:35 +0330

    Replace os.kill signal to syscall.SIGTERM

--- server.go
+++ server.go
@@ -14,6 +14,7 @@ import (
 	"net/http"
 	"os"
 	"os/signal"
+	"syscall"
 	"time"
 )
 
@@ -79,7 +80,7 @@ func main() {
 	)
 
 	sig := make(chan os.Signal, 1)
-	signal.Notify(sig, os.Interrupt, os.Kill)
+	signal.Notify(sig, os.Interrupt, syscall.SIGTERM)
 	go func() {
 		<-sig
 		fmt.Printf("Got interrupt Signal. Cleaning up...\n")
